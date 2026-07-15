247526a7c3aa1aeb9ffd3da361938abe05d30133
Luke Sims <luke@lukesims.uk>
2026-07-15 13:32:58 +0100

    Update shell ssh/keychain setup

--- .shell/ssh.zsh
+++ .shell/ssh.zsh
@@ -1,6 +1,6 @@
 # Don't open a GUI to ask for passwords
 unset SSH_ASKPASS
-unset SSH_ASKPASS_REQUIRE
+export SSH_ASKPASS_REQUIRE=never
 
 # Load our keys into the keychain
-eval $(keychain --quiet --noask --eval id_ed25519)
+eval $(keychain --quiet --noask --nogui --eval id_ed25519)
